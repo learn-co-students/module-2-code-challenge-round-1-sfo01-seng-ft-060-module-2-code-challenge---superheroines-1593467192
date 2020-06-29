e4493d9e20eeeb17c39fdd8fdf05386e2533ccf7
haleighdalke <haleighdalke@gmail.com>
2020-06-29 19:32:21 -0400

    haleigh-dalke-finished

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,37 @@
 class HeroinesController < ApplicationController
+  
+  before_action :set_heroine, only: [:show]
+
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(strong_params)
+    # byebug
+    if @heroine.save
+      redirect_to @heroine
+    else
+      # puts @heroine.errors
+      # render :new
+      flash[:messages] = @heroine.errors.full_messages
+      render :new
+    end
+
+  end
+
+  private
+
+  def strong_params
+    params.require(:heroine).permit(:name, :super_name)
+  end
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,8 @@
 class Power < ApplicationRecord
+    has_many :heroine_powers
+    has_many :heroines, through: :heroine_powers
+
+    def count_heroines
+        self.heroines.count
+    end
 end
