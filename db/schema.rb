117369a285d4936fd4a3413c299b8eafb97a6e41
Guosa_Edebiri <gedebiri@gmail.com>
2020-06-29 16:33:30 -0700

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,23 @@
 
 ActiveRecord::Schema.define(version: 20170725185638) do
 
+  create_table "heroine_powers", force: :cascade do |t|
+    t.string "heroine"
+    t.string "power"
+  end
+
   create_table "heroines", force: :cascade do |t|
-    t.string   "name"
-    t.string   "super_name"
+    t.string "name"
+    t.string "super_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "powers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
